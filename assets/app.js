2e38d2e39a365a07f533fedeeb7d7528c2fb501c
juanlozano2000 <juaniloza83@gmail.com>
2021-07-26 20:47:54 -0300

    carousel de productos para pc

--- assets/app.js
+++ assets/app.js
@@ -2,7 +2,7 @@ window.addEventListener("load", function () {
     new Glider(document.querySelector('.carousel__lista'), {
         slidesToScroll: 1,
         slidesToShow: 5.5,
-        draggable: false,
+        draggable: true,
         dots: '.carousel__indicadores',
         arrows: {
           prev: '.carousel__anterior',
