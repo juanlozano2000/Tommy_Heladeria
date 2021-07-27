34b957ceebc43da012ebd97511a7eb65b64abef3
juanlozano2000 <juaniloza83@gmail.com>
2021-07-27 15:21:32 -0300

    Inicio terminado

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,19 @@
+// Carousel de nuestros SABORES
+window.addEventListener("load", function () {
+  new Glider(document.querySelector('.carousel__lista2'), {
+      slidesToScroll: 1,
+      slidesToShow: 5.5,
+      draggable: true,
+      dots: '.carousel__indicadores2',
+      arrows: {
+        prev: '.carousel__anterior2',
+        next: '.carousel__siguiente2'
+      }
+    });
+})
+
+
+// Carousel de nuestros PRODUCTOS
 window.addEventListener("load", function () {
     new Glider(document.querySelector('.carousel__lista'), {
         slidesToScroll: 1,
